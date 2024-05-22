6b6e2742d2084dbf0173e893b3c1b45895a9af17
Amit paudel <amitpaudel2023@gmail.com>
2024-05-22 15:45:18 +0545

    modified goto top btn

--- src/app/Components/GoTop.jsx
+++ src/app/Components/GoTop.jsx
@@ -1,21 +1,32 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import { IoIosArrowUp } from "react-icons/io";
 
 const GoTop = () => {
+  const [showBtn, setShowBtn] = useState(false);
   const handleGoTop = () => {
     document.documentElement.scrollTo({
       top: 0,
       behavior: "smooth",
     });
   };
+
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 200) {
+      setShowBtn(true);
+    } else setShowBtn(false);
+  });
   return (
-    <button
-      className=" text-xl bg-red-500 shadow-sm shadow-red-600 rounded-full text-white p-3 fixed  bottom-14 right-6 z-[999999]"
-      onClick={handleGoTop}
-    >
-      <IoIosArrowUp />{" "}
-    </button>
+    <>
+      {showBtn && (
+        <button
+          className=" text-xl bg-red-500 shadow-sm shadow-red-600 rounded-full text-white p-3 fixed  bottom-14 right-14 z-[999999]"
+          onClick={handleGoTop}
+        >
+          <IoIosArrowUp />{" "}
+        </button>
+      )}
+    </>
   );
 };
 
